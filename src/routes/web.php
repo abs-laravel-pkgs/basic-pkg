475c19a07812dcd2301677931b6810495f9c94d1
Abdul <abdul@uitouxsolutions.com>
2019-10-21 12:34:54 +0530

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
 Route::group(['namespace' => 'Abs\Basic', 'middleware' => ['web'], 'prefix' => 'admin'], function () {
-
 	Route::get('/admin/master/entity/list/{entity_type_id}', 'EntityController@entityList')->name('entityList');
 });
