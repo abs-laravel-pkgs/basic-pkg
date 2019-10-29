7dfaca3844d92804ee0a2158328fec063cbc82aa
Abdul <abdul@uitouxsolutions.com>
2019-10-29 18:11:27 +0530

    Update AuthController.php

--- src/controllers/API/AuthController.php
+++ src/controllers/API/AuthController.php
@@ -34,11 +34,6 @@ class AuthController extends Controller {
 				}
 			}
 		}
-		// $user = User::with([
-		// 	'entity',
-		// ])
-		// 	->find(Auth::user()->id);
-		// dd($user);
 		$user = Auth::user();
 		$user->permissions = $user->perms();
 		$user->token = $user->createToken('mobile_v2')->accessToken;
