505ed80a2765d30652f569e7147f46379546d280
Abdul <abdul@uitouxsolutions.com>
2020-04-06 02:29:59 +0530

    u

--- src/controllers/AuthedAngularPageController.php
+++ src/controllers/AuthedAngularPageController.php
@@ -8,6 +8,10 @@ class AuthedAngularPageController extends Controller {
 
 	public function authedAngularPage(Request $r) {
 		$this->data['theme'] = config('custom.theme');
-		return view('themes/' . $this->data['theme'] . '/pages/authed-angular-page', $this->data);
+		return view('themes/' . $this->data['theme'] . '/authed-angular-page', $this->data);
+	}
+
+	public function home(Request $r) {
+		return redirect()->route('home');
 	}
 }
