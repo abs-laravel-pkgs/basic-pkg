d4be1cc5ead095697d9bf3e667b833c3048a326b
Abdul <abdul@uitouxsolutions.com>
2020-05-28 01:21:50 +0530

    Update Config.php

--- src/models/Config.php
+++ src/models/Config.php
@@ -30,4 +30,30 @@ class Config extends Model {
 		return $list;
 	}
 
+	public static function getDropDownList($params = []) {
+		$list = Self::select([
+			'id',
+			'name',
+		])
+			->where(function ($q) use ($params) {
+				if (isset($params['config_type_id'])) {
+					$q->where('config_type_id', $params['config_type_id']);
+				}
+			})
+			->orderBy('name');
+		$list = collect($list->get());
+		if (!isset($params['add_default'])) {
+			$params['add_default'] = true;
+		}
+		if (isset($params['add_default']) && $params['add_default']) {
+			if (isset($params['default_text']) && $params['default_text']) {
+				$default_text = $params['default_text'];
+			} else {
+				$default_text = 'Select';
+			}
+			$list->prepend(['id' => '', 'name' => $default_text]);
+		}
+		return $list;
+	}
+
 }
